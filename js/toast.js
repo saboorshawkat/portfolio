4ab615cfefbf96a7ffd0044aea2de75d4f5917f1
Saboor Shawkat <saboorshawkat@proton.me>
2026-09-01 19:31:29 +0530

    Delete js directory

--- js/toast.js
+++
@@ -1,9 +0,0 @@
-  // ---- Toast helper ----
-  const toastEl = document.getElementById('toast');
-  let toastTimer;
-  function showToast(msg){
-    toastEl.textContent = msg;
-    toastEl.classList.add('show');
-    clearTimeout(toastTimer);
-    toastTimer = setTimeout(() => toastEl.classList.remove('show'), 2200);
-  }
