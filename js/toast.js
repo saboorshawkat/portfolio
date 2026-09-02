330e93d018470f24f2235c8693f4dc2fac95996e
Saboor Shawkat <saboorshawkat@proton.me>
2026-09-02 08:20:49 +0530

    Add toast helper function for notifications

---
+++ js/toast.js
@@ -0,0 +1,9 @@
+  // ---- Toast helper ----
+  const toastEl = document.getElementById('toast');
+  let toastTimer;
+  function showToast(msg){
+    toastEl.textContent = msg;
+    toastEl.classList.add('show');
+    clearTimeout(toastTimer);
+    toastTimer = setTimeout(() => toastEl.classList.remove('show'), 2200);
+  }
